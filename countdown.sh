049ad267d8d7e39dec2b24db3b8235de40a84684
dwibnrsl 99 <nursallynadwibuana@gmail.com>
2024-10-28 08:02:05 +0000

    DWI BUANA NURSALLYNA_2404008

---
+++ countdown.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo -e "\n~~ Countdown Timer ~~\n"
+if [[ $1 -gt 0 ]]
+then
+ : '
+for (( i = $1; i >= 0; i-- ))
+while [[ $I -ge 0 ]]
+do
+  echo $I
+  (( I-- ))
+  sleep 1
+done
+I=$1
+else
+  echo Include a positive integer as the first argument.
+fi
